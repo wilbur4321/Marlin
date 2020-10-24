8472c8d887086688e4574b4247cdb4de40e50fe8
Randy Chapman <wilbur4321@gmail.com>
2020-10-23 21:20:03 -0700

    Working better

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1038,8 +1038,8 @@
  * A total of 2 does fast/slow probes with a weighted average.
  * A total of 3 or more adds more slow probes, taking the average.
  */
-#define MULTIPLE_PROBING 4
-#define EXTRA_PROBING    2
+#define MULTIPLE_PROBING 2
+#define EXTRA_PROBING    0
 
 /**
  * Z probes require clearance when deploying, stowing, and moving between
